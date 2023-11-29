2c1cec6b0dbca0410ac23041b4867f6cabbf0e30
yassine w <yassine3107@hotmail.com>
2023-11-29 23:44:47 +0100

    pkg.json

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,10 @@ import Hero from '@/components/hero/Hero'
 import About from '@/components/about/About'
 import Contact from '@/components/contact/Contact'
 
+export const metadata = {
+  title: "I specialize in websites and desktop apps, transforming ideas into sleek, interactive, and user-friendly experiences.",
+  description: "Welcome to my corner of the digital realm. I specialize in websites and desktop apps, transforming ideas into sleek, interactive, and user-friendly experiences. Join me on a journey where innovation meets functionality."
+};
 
 export default function Home() {
   return (
